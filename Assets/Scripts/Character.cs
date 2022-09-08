6c2cb9e0f19c2e8249b771ab1f4481010d9dd3c5
Canklot <mustafacandan211@gmail.com>
2022-09-08 20:56:41 +0300

    removed new input system

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,45 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     private CharacterController _characterController;
     private Animator _animator;
-    InputController inputControllerIns;
-
-    private bool _isGrounded;
-    bool isMoventPressed;
-    int isWalkingHash;
-
-    Vector3 currentMovementInput;
-    Vector2 currentMovement;
+    private int _isWalkingHash;
 
     void Awake()
     {
         _characterController = gameObject.GetComponent<CharacterController>();
         _animator = gameObject.GetComponent<Animator>();
-        inputControllerIns = new InputController();
+        _isWalkingHash = Animator.StringToHash("isWalking");
 
-        isWalkingHash = Animator.StringToHash("isWalking");
-
-        inputControllerIns.CharacterControls.Move.started += onMovementInput;
-        inputControllerIns.CharacterControls.Move.canceled += onMovementInput;
-        inputControllerIns.CharacterControls.Move.performed += onMovementInput;
     }
-
-    void OnEnable()
-    {
-        inputControllerIns.CharacterControls.Enable();
-    }
-
-    void OnDisable()
-    {
-        inputControllerIns.CharacterControls.Disable();
-    }
-
-    void Start() { }
 
     void Update()
     {
@@ -49,30 +24,18 @@ public class Character : MonoBehaviour
 
     void Move()
     {
-        //_characterController.Move((Vector3.forward * Time.deltaTime));
-        _characterController.Move(new Vector3(Vector3.forward, currentMovement));
-        //(Vector3.forward * Time.deltaTime)+(currentMovement * Time.deltaTime));
-    }
+        if (Input.GetKey("d"))
+        {
+            print("D key was pressed");
+        }
+        _characterController.Move(Vector3.forward * Time.deltaTime);
 
-    void onMovementInput(InputAction.CallbackContext context)
-    {
-        currentMovementInput = context.ReadValue<Vector2>();
-        currentMovement.x = currentMovementInput.x;
-        currentMovement.y = currentMovementInput.y;
-        isMoventPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
     }
 
     void handleAnimation()
     {
-        bool isWalking = _animator.GetBool(isWalkingHash);
+        bool isWalking = _animator.GetBool(_isWalkingHash);
 
-        if (isMoventPressed && !isWalking)
-        {
-            _animator.SetBool("isWalking", true);
-        }
-        else if (!isMoventPressed && isWalking)
-        {
-            _animator.SetBool("isWalking", false);
-        }
+        //_animator.SetBool("isWalking", true);
     }
 }
