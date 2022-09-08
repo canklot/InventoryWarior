9389b6752d86a4a9f1e038f552a4c385923beb5a
Canklot <mustafacandan211@gmail.com>
2022-09-08 20:30:57 +0300

    walks rigt left and animation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,58 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
-
 
 public class Character : MonoBehaviour
 {
-
-    [SerializeField] private float _moveSpeed = 6.0f;
-    [SerializeField] private float _jumpForce = 200;
-
     private CharacterController _characterController;
     private Animator _animator;
+    InputController inputControllerIns;
+
+    private bool _isGrounded;
+    bool isMoventPressed;
+    int isWalkingHash;
+
+    Vector3 currentMovementInput;
+    Vector2 currentMovement;
+
     void Awake()
     {
-
         _characterController = gameObject.GetComponent<CharacterController>();
+        _animator = gameObject.GetComponent<Animator>();
+        inputControllerIns = new InputController();
 
+        isWalkingHash = Animator.StringToHash("isWalking");
+
+        inputControllerIns.CharacterControls.Move.started += onMovementInput;
+        inputControllerIns.CharacterControls.Move.canceled += onMovementInput;
+        inputControllerIns.CharacterControls.Move.performed += onMovementInput;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-
+        inputControllerIns.CharacterControls.Enable();
     }
 
-    // Update is called once per frame
+    void OnDisable()
+    {
+        inputControllerIns.CharacterControls.Disable();
+    }
+
+    void Start() { }
+
     void Update()
     {
         Move();
+        handleAnimation();
     }
+
     void Move()
     {
-        _characterController.Move(Vector3.forward * Time.deltaTime);
-
-        //_rigidBody.velocity = characterVelocity;
-
+        //_characterController.Move((Vector3.forward * Time.deltaTime));
+        _characterController.Move(new Vector3(Vector3.forward, currentMovement));
+        //(Vector3.forward * Time.deltaTime)+(currentMovement * Time.deltaTime));
     }
-    void HandleGravity()
+
+    void onMovementInput(InputAction.CallbackContext context)
     {
-        if (Grounded)
-        {
-            if (_verticalVelocity < 0.0f)
-            {
-                _verticalVelocity = 0.0f;
-            }
-        }
-        if (IsJumping)
-        {
-            _verticalVelocity = 0f;
-        }
-        else if (_verticalVelocity < _terminalVelocity)
-        {
-            _verticalVelocity += Gravity * Time.deltaTime;
-        }
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.y = currentMovementInput.y;
+        isMoventPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
     }
 
+    void handleAnimation()
+    {
+        bool isWalking = _animator.GetBool(isWalkingHash);
+
+        if (isMoventPressed && !isWalking)
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        else if (!isMoventPressed && isWalking)
+        {
+            _animator.SetBool("isWalking", false);
+        }
+    }
 }
