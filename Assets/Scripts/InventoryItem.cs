a6ccf7b02d5c638ac0f4c978f0e90cd6f0a1e87b
Canklot <mustafacandan211@gmail.com>
2022-12-27 22:43:53 +0300

    ResizeGrid compatible with fit to width

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class InventoryItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int sizeWidth;
+    public int sizeHeight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Start() { }
+
+    void Update() { }
 }
--- Assets/Scripts/ItemGrid.cs
+++ Assets/Scripts/ItemGrid.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemGrid : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class ItemGrid : MonoBehaviour
     // Looks like it also works when its set to strech, anchor and pivot set to bottom mid
     [SerializeField]
     Canvas InventoryCanvas;
+    RectTransform InventoryCanvasRect;
 
     float TileSizeWidth;
     float TileSizeHeight;
@@ -24,13 +26,14 @@ public class ItemGrid : MonoBehaviour
     private void Start()
     {
         GridRectTransform = GetComponent<RectTransform>();
-        RectTransform InventoryCanvasRect = InventoryCanvas.GetComponent<RectTransform>();
+        InventoryCanvasRect = InventoryCanvas.GetComponent<RectTransform>();
 
         TileSizeWidth = Screen.width / InventoryCanvasRect.rect.width;
         TileSizeWidth = TileSizeWidth * ReferancePixelPerUnit;
         // When set to fit width, height and with are the same
         TileSizeHeight = TileSizeWidth;
         //Debug.Log("tile width " + TileSizeWidth + " tile height " + TileSizeHeight);
+        ResizeGrid(600, 800);
     }
 
     public Vector2Int GetTileGridPosition(Vector2 MousePosition)
@@ -43,5 +46,21 @@ public class ItemGrid : MonoBehaviour
         TileCoordinates.y = (int)(GridCoordinates.y / TileSizeHeight);
 
         return TileCoordinates;
+    }
+
+    void ResizeGrid(int ResizeWidth, int ResizeHeight)
+    {
+        //In the video this function is init
+
+        // Because grid is set to fit width you can only change how many tiles on top of each other by changing grid height
+        // If you want to change number of horizontal tiles you need to chane gridcanvas width
+        // Example: Referance pixel per unit on GridCanvas is 100 if you want 8 horizontal tiles set GridCanvas width to 800
+        // I dont know if canvas height matters
+
+        CanvasScaler InventoryCanvasScaler = InventoryCanvas.GetComponent<CanvasScaler>();
+        // Change horizontal number of tiles
+        InventoryCanvasScaler.referenceResolution = new Vector2(ResizeWidth, ResizeHeight);
+        // Change vertical number of tiles
+        GridRectTransform.sizeDelta = new Vector2(1, ResizeHeight);
     }
 }
